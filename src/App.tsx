a89113c753053f056f4ee6669bc174a76713b6be
DeadRabbit <309358965@qq.com>
2024-07-22 17:55:49 +0800

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { OrbitControls, View } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
-import { ReactNode, Suspense } from "react";
+import { ReactNode, Suspense, useEffect } from "react";
 
 import { Hero } from "./components/Hero";
 import { Item1 } from "./grid/Item1";
@@ -18,23 +18,61 @@ import { Item12 } from "./grid/Item12";
 
 const Wrapper = ({ children }: { children: ReactNode }) => {
   return (
-    <View
-      className="flex items-center from-[#111111] to-[#181818] bg-gradient-to-br justify-center duration-300 border rounded-md hover:transition-opacity text-white/20 border-white/10 hover:border-white/10 aspect-square hover:bg-white/5"
-      style={{ position: "relative", overflow: "hidden" }}
-    >
-      {children}
-    </View>
+    <div className="relative rounded-md cursor-pointer aspect-square bg-white/10 card">
+      <View
+        className="flex z-[2] m-[1px] bg-[#171717] h-full w-full  relative rounded-md "
+        style={{
+          height: `calc(100% - 2px)`,
+          width: `calc(100% - 2px)`,
+        }}
+      >
+        {children}
+      </View>
+      <div
+        className="absolute top-0 left-0 w-full h-full transition-opacity duration-500 rounded-md opacity-0 group-hover:opacity-100 z-1"
+        style={{
+          background: `radial-gradient(400px circle at var(--mouse-x) var(--mouse-y),rgba(255, 255, 255, 0.3),transparent 40%)`,
+        }}
+      ></div>
+    </div>
   );
 };
 
 function App() {
+  useEffect(() => {
+    const cards = document.querySelectorAll<HTMLDivElement>(".card");
+
+    const handlePointerMove = (e: PointerEvent) => {
+      cards.forEach((card) => {
+        const rect = card.getBoundingClientRect();
+        const x = e.clientX - rect.left;
+        const y = e.clientY - rect.top;
+
+        card.style.setProperty("--mouse-x", `${x}px`);
+        card.style.setProperty("--mouse-y", `${y}px`);
+      });
+    };
+
+    document
+      .querySelector<HTMLDivElement>("[data-gird]")
+      ?.addEventListener("pointermove", handlePointerMove);
+
+    return () => {
+      document
+        .querySelector<HTMLDivElement>("[data-gird]")
+        ?.removeEventListener("pointermove", handlePointerMove);
+    };
+  }, []);
+
   return (
     <div className="min-h-screen text-white bg-[#0c0c0c] select-none background">
       <div className="container p-5 pb-20 mx-auto ">
         <Hero></Hero>
-
         <div className="relative mt-5 overflow-hidden">
-          <div className="grid h-full gap-5 overflow-hidden grid-clos-1 md:grid-cols-2 lg:grid-cols-4 ">
+          <div
+            className="grid h-full gap-5 overflow-hidden group grid-clos-1 md:grid-cols-2 lg:grid-cols-4"
+            data-gird
+          >
             <Wrapper>
               <Item1></Item1>
             </Wrapper>
@@ -73,7 +111,7 @@ function App() {
             </Wrapper>
           </div>
 
-          <div className="fixed top-0 left-0 w-full h-screen pointer-events-none ">
+          <div className="fixed top-0 left-0 z-20 w-full h-screen pointer-events-none ">
             <Canvas
               camera={{
                 zoom: 0.8,
